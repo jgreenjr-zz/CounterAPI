747df984dbea6cf27c70225286e505cc95ef4b57
jgreenjr <jgreenjr@gmail.com>
2017-03-12 15:51:55 -0400

    parsing body

--- src/counter_post.py
+++ src/counter_post.py
@@ -4,15 +4,16 @@ import io
 
 
 def handler(event, context):
+  body = json.loads(event['body'])
   dynamodb = boto3.resource('dynamodb')
   
   table = dynamodb.Table('MyTable')
                                               
   table.put_item({
-    'UserName': event['UserName']
+    'UserName': body['UserName']
   })
   
-  responseBody = {"message":"success"}
+  responseBody = {"message":"Created {0}".format(body['UserName'])}
 
   return  {
     'statusCode':201,
