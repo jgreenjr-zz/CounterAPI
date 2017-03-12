368167e72dd8f117b8935cf8d4c4f1562b6056d4
jgreenjr <jgreenjr@gmail.com>
2017-03-12 14:40:47 -0400

    first attempt at trying to access table

--- src/counter_get.py
+++ src/counter_get.py
@@ -1,8 +1,19 @@
+import json
+import boto3
+import io
 
 
 def handler(event, context):
-   message = 'Hello'
-   return  {
-     'message' : message
-   }
-   
+  dynamodb = boto3.resource('dynamodb')
+  
+  table = dynamodb.Table('MyTable')
+                                              
+  results = table.scan()
+  
+  responseBody = result["Items"]
+
+  return  {
+    'statusCode':200,
+    'headers':{},
+    'body':json.dumps(responseBody)
+  }
